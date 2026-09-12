86ae20db927c12f42fb3e2eddf18634c0eca91a7
shimez <ctake.shimez@gmail.com>
2026-09-12 19:39:56 +0900

    Release v1.12.2

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 #include <Arduino.h>
 #include "M5Chain.h"
 
-static const char* APP_VERSION = "1.12.1";
+static const char* APP_VERSION = "1.12.2";
 
 // ---------------------------------------------------------------------------
 // Hardware / bus
